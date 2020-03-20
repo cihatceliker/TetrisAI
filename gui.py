rotation system fixed

gui.py
@@ -3,7 +3,7 @@ import threading
 import numpy as np
 import random
 import pickle
-from tetris import Environment
+from environment import Environment
 from tkinter import Frame, Canvas, Tk
 
 BACKGROUND_COLOR = "#000"
@@ -11,7 +11,7 @@ PIECE_COLOR = "#fff"
 
 class GameGrid():
 
-    def __init__(self, speed=0.3, size=720):
+    def __init__(self, speed=0.1, size=720):
         width = size / 2
         height = size
         self.root = Tk()
@@ -28,7 +28,13 @@ class GameGrid():
         self.rectangle_size = size/self.env.row
         self.pause = True
         self.quit = False
-
+        self.commands = {
+            113: 1, # Left
+            114: 2, # Right
+            53: 3, # Z
+            52: 4, # X
+            65: 5 # Drop
+        }
         self.init()
         self.root.title('Tetris')
         self.root.bind("<Key>", self.key_down)
@@ -42,15 +48,13 @@ class GameGrid():
         action = 0
         while not self.quit:
             #action = self.agent.select_action(state)
-            #action = np.random.randint(5)
-            next_state, reward, done, info = self.env.step(2)
-
+            action = np.random.randint(5)
+            next_state, reward, done, info = self.env.step(action)
             score += reward
             if done:
                 self.env.reset()
             self.update()
             time.sleep(self.speed)
-
         print("done", score)
 
     def update(self):
@@ -59,7 +63,7 @@ class GameGrid():
                 rect = self.game_area[i][j]
                 curr = int(self.env.board[i, j])
                 color = BACKGROUND_COLOR if curr == 0 else PIECE_COLOR
-                #if i == self.env.rel_x and j == self.env.rel_y: color = "blue"
+                if i == self.env.rel_x and j == self.env.rel_y: color = "blue"
                 self.game.itemconfig(rect, fill=color)
 
     def init(self):
@@ -77,21 +81,12 @@ class GameGrid():
             self.game_area.append(row)
     
     def key_down(self, event):
-        key = repr(event.char)
-        if key == "'q'":
+        if event.keycode == 24: # q
             self.quit = True
-        if event.keycode == 114:
-            self.env._move(True)
-        if event.keycode == 113:
-            self.env._move(False)
-        if key == "'z'":
-            self.env._rotate(True)
-        if key == "'x'":
-            self.env._rotate(False)
-        if key == "' '":
-            self.env.drop()
+        if not self.env.is_action and event.keycode in self.commands:
+            self.env.apply_action(self.commands[event.keycode])
         self.update()
-        print(event)
+
 
 if __name__ == "__main__":
     GameGrid()